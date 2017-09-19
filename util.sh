cc9daee9dd5f5b71112b89bb482b7819b232a3a9
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-09-19 08:53:20 +0200

    worked on util.sh

--- util.sh
+++ util.sh
@@ -7,11 +7,11 @@ set -e
 if [ "$1" == "docker-build" ]; then
 
     mvn clean install -DskipTests
-    mvn -f -P docker
+    mvn docker:build -P docker
 
 elif [ "$1" == "docker-push" ]; then
 
-    docker push nimbleplatform/frontend-service:latest
+    docker push nimbleplatform/business-process-service:latest
 
 fi
 
