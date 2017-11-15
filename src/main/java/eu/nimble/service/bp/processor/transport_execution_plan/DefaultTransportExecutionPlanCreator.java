0f62265c993cec3e2f35f44703e7cb4520284a82
Suat Gonul <suat@srdc.com.tr>
2017-11-15 16:33:12 +0300

    transport execution plan classes

--- src/main/java/eu/nimble/service/bp/processor/transport_execution_plan/DefaultTransportExecutionPlanCreator.java
+++ src/main/java/eu/nimble/service/bp/processor/transport_execution_plan/DefaultTransportExecutionPlanCreator.java
@@ -2,9 +2,12 @@ package eu.nimble.service.bp.processor.transport_execution_plan;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import eu.nimble.service.bp.application.IBusinessProcessApplication;
+import eu.nimble.service.bp.config.BusinessProcessPersistenceConfig;
 import eu.nimble.service.bp.impl.util.persistence.DocumentDAOUtility;
 import eu.nimble.service.bp.swagger.model.ExecutionConfiguration;
 import eu.nimble.service.bp.swagger.model.ProcessConfiguration;
+import eu.nimble.service.bp.swagger.model.ProcessDocumentMetadata;
+import eu.nimble.service.model.ubl.transportexecutionplan.TransportExecutionPlanType;
 import eu.nimble.service.model.ubl.transportexecutionplanrequest.TransportExecutionPlanRequestType;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
@@ -13,33 +16,34 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
-public class DefaultTransportExecutionPlanRequestSender implements JavaDelegate {
+public class DefaultTransportExecutionPlanCreator implements JavaDelegate {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @HystrixCommand
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        logger.info(" $$$ DefaultTransportExecutionPlanRequestSender: {}", execution);
+
+        BusinessProcessPersistenceConfig config = BusinessProcessPersistenceConfig.getInstance();
+
+        logger.info(" $$$ DefaultTransportExecutionPlanCreator: {}", execution);
         final Map<String, Object> variables = execution.getVariables();
         // for debug purposes
-        for (String key : variables.keySet()) {
+        for (String key: variables.keySet()) {
             logger.debug(" $$$ Variable name {}, value {}", key, variables.get(key));
         }
-
-        // get process instance id
-        String processInstanceId = execution.getProcessInstance().getProcessInstanceId();
-
         // get input variables
         String buyer = variables.get("initiatorID").toString();
         String seller = variables.get("responderID").toString();
-        TransportExecutionPlanRequestType order = (TransportExecutionPlanRequestType) variables.get("transportExecutionPlanRequest");
+        String content = variables.get("content").toString();
 
         // get application execution configuration
         ExecutionConfiguration executionConfiguration = DocumentDAOUtility.getExecutionConfiguration(buyer,
-                execution.getProcessInstance().getProcessDefinitionId(), ProcessConfiguration.RoleTypeEnum.BUYER, "TRANSPORT_EXECUTION_PLAN_REQUEST",
-                ExecutionConfiguration.ApplicationTypeEnum.DATACHANNEL);
+                execution.getProcessInstance().getProcessDefinitionId(), ProcessConfiguration.RoleTypeEnum.SELLER, "TRANSPORT_EXECUTION_PLAN", ExecutionConfiguration.ApplicationTypeEnum.DATAADAPTER);
         String applicationURI = executionConfiguration.getExecutionUri();
         ExecutionConfiguration.ExecutionTypeEnum executionType = executionConfiguration.getExecutionType();
+
+         // specify output variables
+        TransportExecutionPlanType transportExecutionPlan = null;
 
         // Call that configured application with the variables
         if(executionType == ExecutionConfiguration.ExecutionTypeEnum.JAVA) {
@@ -48,11 +52,14 @@ public class DefaultTransportExecutionPlanRequestSender implements JavaDelegate 
 
             IBusinessProcessApplication businessProcessApplication = (IBusinessProcessApplication) instance;
 
-            businessProcessApplication.sendDocument(processInstanceId, buyer, seller, order);
+            transportExecutionPlan  = (TransportExecutionPlanType) businessProcessApplication.createDocument(buyer, seller, content, ProcessDocumentMetadata.TypeEnum.TRANSPORTEXECUTIONPLAN);
         } else if(executionType == ExecutionConfiguration.ExecutionTypeEnum.MICROSERVICE) {
             // TODO: How to call a microservice
         } else {
             // TODO: think other types of execution possibilities
         }
+
+        // set the corresponding camunda business process variable
+        execution.setVariable("transportExecutionPlan", transportExecutionPlan);
     }
 }
--- src/main/java/eu/nimble/service/bp/processor/transport_execution_plan/DefaultTransportExecutionPlanProcessor.java
+++ src/main/java/eu/nimble/service/bp/processor/transport_execution_plan/DefaultTransportExecutionPlanProcessor.java
@@ -5,6 +5,7 @@ import eu.nimble.service.bp.application.IBusinessProcessApplication;
 import eu.nimble.service.bp.impl.util.persistence.DocumentDAOUtility;
 import eu.nimble.service.bp.swagger.model.ExecutionConfiguration;
 import eu.nimble.service.bp.swagger.model.ProcessConfiguration;
+import eu.nimble.service.model.ubl.transportexecutionplan.TransportExecutionPlanType;
 import eu.nimble.service.model.ubl.transportexecutionplanrequest.TransportExecutionPlanRequestType;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
@@ -13,13 +14,13 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
-public class DefaultTransportExecutionPlanRequestSender implements JavaDelegate {
+public class DefaultTransportExecutionPlanProcessor implements JavaDelegate {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @HystrixCommand
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        logger.info(" $$$ DefaultTransportExecutionPlanRequestSender: {}", execution);
+        logger.info(" $$$ DefaultTransportExecutionPlanProcessor: {}", execution);
         final Map<String, Object> variables = execution.getVariables();
         // for debug purposes
         for (String key : variables.keySet()) {
@@ -32,12 +33,11 @@ public class DefaultTransportExecutionPlanRequestSender implements JavaDelegate 
         // get input variables
         String buyer = variables.get("initiatorID").toString();
         String seller = variables.get("responderID").toString();
-        TransportExecutionPlanRequestType order = (TransportExecutionPlanRequestType) variables.get("transportExecutionPlanRequest");
+        TransportExecutionPlanType transportExecutionPlan = (TransportExecutionPlanType) variables.get("transportExecutionPlan");
 
         // get application execution configuration
-        ExecutionConfiguration executionConfiguration = DocumentDAOUtility.getExecutionConfiguration(buyer,
-                execution.getProcessInstance().getProcessDefinitionId(), ProcessConfiguration.RoleTypeEnum.BUYER, "TRANSPORT_EXECUTION_PLAN_REQUEST",
-                ExecutionConfiguration.ApplicationTypeEnum.DATACHANNEL);
+        ExecutionConfiguration executionConfiguration = DocumentDAOUtility.getExecutionConfiguration(buyer, execution.getProcessInstance().getProcessDefinitionId(), ProcessConfiguration.RoleTypeEnum.SELLER, "TRANSPORT_EXECUTION_PLAN",
+                ExecutionConfiguration.ApplicationTypeEnum.DATAPROCESSOR);
         String applicationURI = executionConfiguration.getExecutionUri();
         ExecutionConfiguration.ExecutionTypeEnum executionType = executionConfiguration.getExecutionType();
 
@@ -48,7 +48,7 @@ public class DefaultTransportExecutionPlanRequestSender implements JavaDelegate 
 
             IBusinessProcessApplication businessProcessApplication = (IBusinessProcessApplication) instance;
 
-            businessProcessApplication.sendDocument(processInstanceId, buyer, seller, order);
+            businessProcessApplication.saveDocument(processInstanceId, buyer, seller, transportExecutionPlan);
         } else if(executionType == ExecutionConfiguration.ExecutionTypeEnum.MICROSERVICE) {
             // TODO: How to call a microservice
         } else {
