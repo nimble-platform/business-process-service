fd6ba587ea9ee3dfff91d989348c0e51b3f4f145
Suat Gonul <suat@srdc.com.tr>
2018-03-30 16:00:51 +0300

    fixed rest url for filters

--- src/main/java/eu/nimble/service/bp/impl/ProcessInstanceGroupController.java
+++ src/main/java/eu/nimble/service/bp/impl/ProcessInstanceGroupController.java
@@ -14,10 +14,11 @@ import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
