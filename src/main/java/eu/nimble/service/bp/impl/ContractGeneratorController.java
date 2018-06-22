2f1d86bf1cf5c811c6d1005beac3ee536b87bae6
dogukan10 <dogukan10cav@gmail.com>
2018-06-22 14:42:28 +0300

    Added service to create contract documents

---
+++ src/main/java/eu/nimble/service/bp/impl/ContractGeneratorController.java
@@ -0,0 +1,54 @@
+package eu.nimble.service.bp.impl;
+
+import eu.nimble.service.bp.impl.contract.ContractGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.zip.ZipOutputStream;
+
+@Controller
+public class ContractGeneratorController {
+    private final Logger logger = LoggerFactory.getLogger(ContractGeneratorController.class);
+
+    @CrossOrigin(origins = {"*"})
+    @RequestMapping(value = "/contract",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            method = RequestMethod.POST,
+            produces = {"application/zip"})
+    public void generateContract(@RequestBody String orderId, HttpServletResponse response){
+        try{
+            logger.info("Generating contract for the order with id : {}",orderId);
+
+            ZipOutputStream zos = new ZipOutputStream(response.getOutputStream());
+
+            ContractGenerator contractGenerator = new ContractGenerator();
+            contractGenerator.generateContract(orderId,zos);
+
+            response.flushBuffer();
+
+            zos.close();
+
+            logger.info("Generated contract for the order with id : {}",orderId);
+
+        }
+        catch (Exception e){
+            logger.error("Failed to generate contract for the order with id : {}",orderId,e);
+
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            try{
+                response.getOutputStream().write("Failed to generate contract".getBytes());
+            }
+            catch (Exception e1){
+                logger.error("Failed to write the error message to the output stream",e1);
+            }
+        }
+
+    }
+
+
+}
