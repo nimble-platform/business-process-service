0eaae8cbae262ed02f2165ab1ed862b3097a57ff
dogukan10 <dogukan10cav@gmail.com>
2018-06-26 15:16:45 +0300

    Created EPCController and changed url of data-channel-service

--- src/main/java/eu/nimble/service/bp/impl/EPCController.java
+++ src/main/java/eu/nimble/service/bp/impl/EPCController.java
@@ -1,6 +1,14 @@
 package eu.nimble.service.bp.impl;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import eu.nimble.service.bp.config.GenericConfig;
 import eu.nimble.service.bp.impl.model.tt.TTInfo;
+import eu.nimble.service.bp.impl.util.persistence.HibernateUtilityRef;
+import eu.nimble.service.bp.impl.util.spring.SpringBridge;
+import eu.nimble.utility.Configuration;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
@@ -10,12 +18,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by suat on 06-Jun-18.
@@ -24,6 +32,27 @@ import java.util.List;
 public class EPCController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    static class EpcCodes {
+        private String orderId;
+        private Set<String> codes = new HashSet<>();
+
+        String getOrderId() {
+            return orderId;
+        }
+
+        void setOrderId(String orderId) {
+            this.orderId = orderId;
+        }
+
+        Set<String> getCodes() {
+            return codes;
+        }
+
+        void setCodes(Set<String> codes) {
+            this.codes = codes;
+        }
+    }
 
     @ApiOperation(value = "Gets product-related and company-related details for the given epc code", response = TTInfo.class)
     @ApiResponses(value = {
@@ -52,7 +81,7 @@ public class EPCController {
         }
     }
 
-    @ApiOperation(value = "Gets productId codes associated with the given product id", response = List.class)
+    @ApiOperation(value = "Gets EPC codes associated with the given product id", response = List.class)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Retrieved the T&T details successfully", response = List.class)})
     @RequestMapping(value = "/t-t/epc-codes",
@@ -75,6 +104,54 @@ public class EPCController {
             String msg = "Unexpected error while getting the epc codes for the productId: %s";
             logger.error(String.format(msg, productId));
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(msg);
+        }
+    }
+
+    @ApiOperation(value = "Gets EPC codes that belongs to the same published product ID", response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retrieved epc codes successfully", response = List.class)})
+    @RequestMapping(value = "/t-t/products/epc-codes",
+            produces = {"application/json"},
+            method = RequestMethod.GET)
+    public ResponseEntity getEPCCodesBelongsToProduct(@ApiParam(value = "The published product ID") @RequestParam(value = "publishedProductID", required = true) String publishedProductID,
+                                                      @ApiParam(value = "The manufacturer party ID") @RequestParam(value = "manufacturerPartyID", required = true) String manufacturerPartyID,
+                                                      @ApiParam(value = "" ,required=true ) @RequestHeader(value="Authorization", required=true) String bearerToken){
+        logger.info("Getting epc codes for manufacturerPartyID: {} and productId: {}", manufacturerPartyID,publishedProductID);
+        try {
+            String query = "select order_.ID from OrderType order_ join order_.orderLine line where line.lineItem.item.manufacturerParty.ID = '"+manufacturerPartyID+"' AND line.lineItem.item.manufacturersItemIdentification.ID = '"+publishedProductID+"'";
+            List<String> orderIds = (List<String>) HibernateUtilityRef.getInstance(Configuration.UBL_PERSISTENCE_UNIT_NAME).loadAll(query);
+
+            String params = "";
+            int size = orderIds.size();
+            for(int i = 0; i < size;i++){
+                if(i != size-1){
+                    params = params + orderIds.get(i) + ",";
+                }
+                else {
+                    params = params + orderIds.get(i);
+                }
+            }
+
+            GenericConfig config = SpringBridge.getInstance().getGenericConfig();
+            String dataChannelServiceUrlStr = config.getDataChannelServiceUrl()+"/epc/list?orders="+params;
+
+            HttpResponse<com.mashape.unirest.http.JsonNode> response = Unirest.get(dataChannelServiceUrlStr)
+                    .header("Authorization", bearerToken).asJson();
+
+            List<String> epcCodes = new ArrayList<>();
+            ObjectMapper objectMapper = new ObjectMapper();
+            List<EpcCodes> epcCodesList = objectMapper.readValue(response.getBody().toString(),new TypeReference<List<EpcCodes>>(){});
+
+            for(EpcCodes epcCodes1 : epcCodesList){
+                epcCodes.addAll(epcCodes1.getCodes());
+            }
+            logger.info("Retrieved epc codes for manufacturerPartyID: {} and productId: {}", manufacturerPartyID,publishedProductID);
+            return ResponseEntity.status(HttpStatus.OK).body(epcCodes);
+        }
+        catch (Exception e){
+            String msg = "Unexpected error while getting the epc codes for manufacturerPartyID: %s and productId: %s";
+            logger.error(String.format(msg, manufacturerPartyID,publishedProductID));
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
 }
