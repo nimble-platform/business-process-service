6f92e8ec3d7d5f8cb26f9e1078b3b40db0df2466
dogukan10 <dogukan10cav@gmail.com>
2018-09-24 21:57:58 +0300

    Better handling of getParty method

--- src/main/java/eu/nimble/service/bp/impl/util/persistence/CatalogueDAOUtility.java
+++ src/main/java/eu/nimble/service/bp/impl/util/persistence/CatalogueDAOUtility.java
@@ -1,12 +1,21 @@
 package eu.nimble.service.bp.impl.util.persistence;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.nimble.service.model.ubl.commonaggregatecomponents.CatalogueLineType;
+import eu.nimble.service.model.ubl.commonaggregatecomponents.PartyType;
 import eu.nimble.service.model.ubl.order.OrderType;
 import eu.nimble.utility.Configuration;
+import eu.nimble.utility.HibernateUtility;
+import eu.nimble.utility.JsonSerializationUtility;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class CatalogueDAOUtility {
+    private static final Logger logger = LoggerFactory.getLogger(CatalogueDAOUtility.class);
 
     public static CatalogueLineType getCatalogueLine(OrderType order){
         String query = "SELECT cl FROM CatalogueLineType cl WHERE cl.ID = '"+order.getOrderLine().get(0).getLineItem().getLineReference().get(0).getLineID()+"' AND cl.goodsItem.item.manufacturerParty.ID = '"+order.getOrderLine().get(0).getLineItem().getItem().getManufacturerParty().getID()+"'";
@@ -20,5 +29,29 @@ public class CatalogueDAOUtility {
     public static List<String> getOrderIds(CatalogueLineType catalogueLine){
         String query = "SELECT order_.ID from OrderType order_ join order_.orderLine line where line.lineItem.item.manufacturerParty.ID = '"+catalogueLine.getGoodsItem().getItem().getManufacturerParty().getID()+"' AND line.lineItem.item.manufacturersItemIdentification.ID = '"+catalogueLine.getGoodsItem().getItem().getManufacturersItemIdentification().getID()+"'";
         return (List<String>) HibernateUtilityRef.getInstance(Configuration.UBL_PERSISTENCE_UNIT_NAME).loadAll(query);
+    }
+
+    public static PartyType getParty(PartyType party){
+        if(party == null){
+            return null;
+        }
+        String query = "SELECT party FROM PartyType party WHERE party.ID = ?";
+        PartyType partyType = HibernateUtility.getInstance(Configuration.UBL_PERSISTENCE_UNIT_NAME).load(query,party.getID());
+        if(partyType == null){
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper = objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            try {
+                JSONObject object = new JSONObject(objectMapper.writeValueAsString(party));
+                JsonSerializationUtility.removeHjidFields(object);
+                party = objectMapper.readValue(object.toString(), PartyType.class);
+            }
+            catch (Exception e){
+                logger.error("Failed to remove hjid fields from the party",e);
+            }
+
+            HibernateUtility.getInstance(Configuration.UBL_PERSISTENCE_UNIT_NAME).persist(party);
+            return party;
+        }
+        return partyType;
     }
 }
--- src/main/java/eu/nimble/service/bp/impl/util/persistence/DataIntegratorUtil.java
+++ src/main/java/eu/nimble/service/bp/impl/util/persistence/DataIntegratorUtil.java
@@ -13,8 +13,6 @@ import eu.nimble.service.model.ubl.receiptadvice.ReceiptAdviceType;
 import eu.nimble.service.model.ubl.requestforquotation.RequestForQuotationType;
 import eu.nimble.service.model.ubl.transportexecutionplan.TransportExecutionPlanType;
 import eu.nimble.service.model.ubl.transportexecutionplanrequest.TransportExecutionPlanRequestType;
-import eu.nimble.utility.Configuration;
-import eu.nimble.utility.HibernateUtility;
 
 public class DataIntegratorUtil {
 
@@ -22,9 +20,9 @@ public class DataIntegratorUtil {
         if(object instanceof OrderType) {
             OrderType order = (OrderType) object;
 
-            PartyType supplierParty = getParty(order.getSellerSupplierParty().getParty());
+            PartyType supplierParty = CatalogueDAOUtility.getParty(order.getSellerSupplierParty().getParty());
             order.getSellerSupplierParty().setParty(supplierParty);
-            PartyType buyerParty = getParty(order.getBuyerCustomerParty().getParty());
+            PartyType buyerParty = CatalogueDAOUtility.getParty(order.getBuyerCustomerParty().getParty());
             order.getBuyerCustomerParty().setParty(buyerParty);
             for (OrderLineType orderLineType : order.getOrderLine()){
                 orderLineType.getLineItem().getItem().setManufacturerParty(supplierParty);
@@ -33,18 +31,18 @@ public class DataIntegratorUtil {
         } else if(object instanceof OrderResponseSimpleType) {
             OrderResponseSimpleType orderResponse = (OrderResponseSimpleType) object;
 
-            PartyType supplierParty = getParty(orderResponse.getSellerSupplierParty().getParty());
+            PartyType supplierParty = CatalogueDAOUtility.getParty(orderResponse.getSellerSupplierParty().getParty());
             orderResponse.getSellerSupplierParty().setParty(supplierParty);
-            PartyType buyerParty = getParty(orderResponse.getBuyerCustomerParty().getParty());
+            PartyType buyerParty = CatalogueDAOUtility.getParty(orderResponse.getBuyerCustomerParty().getParty());
             orderResponse.getBuyerCustomerParty().setParty(buyerParty);
 
 
         } else if(object instanceof PpapRequestType){
             PpapRequestType ppapRequestType = (PpapRequestType) object;
 
-            PartyType supplierParty = getParty(ppapRequestType.getSellerSupplierParty().getParty());
+            PartyType supplierParty = CatalogueDAOUtility.getParty(ppapRequestType.getSellerSupplierParty().getParty());
             ppapRequestType.getSellerSupplierParty().setParty(supplierParty);
-            PartyType buyerParty = getParty(ppapRequestType.getBuyerCustomerParty().getParty());
+            PartyType buyerParty = CatalogueDAOUtility.getParty(ppapRequestType.getBuyerCustomerParty().getParty());
             ppapRequestType.getBuyerCustomerParty().setParty(buyerParty);
 
             ppapRequestType.getLineItem().getItem().setManufacturerParty(supplierParty);
@@ -52,17 +50,17 @@ public class DataIntegratorUtil {
         } else if(object instanceof PpapResponseType){
             PpapResponseType ppapResponseType = (PpapResponseType) object;
 
-            PartyType supplierParty = getParty(ppapResponseType.getSellerSupplierParty().getParty());
+            PartyType supplierParty = CatalogueDAOUtility.getParty(ppapResponseType.getSellerSupplierParty().getParty());
             ppapResponseType.getSellerSupplierParty().setParty(supplierParty);
-            PartyType buyerParty = getParty(ppapResponseType.getBuyerCustomerParty().getParty());
+            PartyType buyerParty = CatalogueDAOUtility.getParty(ppapResponseType.getBuyerCustomerParty().getParty());
             ppapResponseType.getBuyerCustomerParty().setParty(buyerParty);
 
         }  else if(object instanceof RequestForQuotationType) {
             RequestForQuotationType rfq = (RequestForQuotationType) object;
 
-            PartyType supplierParty = getParty(rfq.getSellerSupplierParty().getParty());
+            PartyType supplierParty = CatalogueDAOUtility.getParty(rfq.getSellerSupplierParty().getParty());
             rfq.getSellerSupplierParty().setParty(supplierParty);
-            PartyType buyerParty = getParty(rfq.getBuyerCustomerParty().getParty());
+            PartyType buyerParty = CatalogueDAOUtility.getParty(rfq.getBuyerCustomerParty().getParty());
             rfq.getBuyerCustomerParty().setParty(buyerParty);
 
             for (RequestForQuotationLineType request : rfq.getRequestForQuotationLine()){
@@ -72,9 +70,9 @@ public class DataIntegratorUtil {
         } else if(object instanceof QuotationType) {
             QuotationType quotation = (QuotationType) object;
 
-            PartyType supplierParty = getParty(quotation.getSellerSupplierParty().getParty());
+            PartyType supplierParty = CatalogueDAOUtility.getParty(quotation.getSellerSupplierParty().getParty());
             quotation.getSellerSupplierParty().setParty(supplierParty);
-            PartyType buyerParty = getParty(quotation.getBuyerCustomerParty().getParty());
+            PartyType buyerParty = CatalogueDAOUtility.getParty(quotation.getBuyerCustomerParty().getParty());
             quotation.getBuyerCustomerParty().setParty(buyerParty);
 
             for (QuotationLineType quotationLineType: quotation.getQuotationLine()){
@@ -84,13 +82,13 @@ public class DataIntegratorUtil {
         } else if(object instanceof DespatchAdviceType) {
             DespatchAdviceType despatchAdvice = (DespatchAdviceType) object;
 
-            PartyType supplierParty = getParty(despatchAdvice.getDespatchSupplierParty().getParty());
+            PartyType supplierParty = CatalogueDAOUtility.getParty(despatchAdvice.getDespatchSupplierParty().getParty());
             despatchAdvice.getDespatchSupplierParty().setParty(supplierParty);
-            PartyType buyerParty = getParty(despatchAdvice.getDeliveryCustomerParty().getParty());
+            PartyType buyerParty = CatalogueDAOUtility.getParty(despatchAdvice.getDeliveryCustomerParty().getParty());
             despatchAdvice.getDeliveryCustomerParty().setParty(buyerParty);
 
             for (DespatchLineType despatchLineType: despatchAdvice.getDespatchLine()){
-                PartyType manufacturerParty = getParty(despatchLineType.getItem().getManufacturerParty());
+                PartyType manufacturerParty = CatalogueDAOUtility.getParty(despatchLineType.getItem().getManufacturerParty());
                 despatchLineType.getItem().setManufacturerParty(manufacturerParty);
             }
 
@@ -98,13 +96,13 @@ public class DataIntegratorUtil {
         } else if(object instanceof ReceiptAdviceType) {
             ReceiptAdviceType receiptAdvice = (ReceiptAdviceType) object;
 
-            PartyType supplierParty = getParty(receiptAdvice.getDespatchSupplierParty().getParty());
+            PartyType supplierParty = CatalogueDAOUtility.getParty(receiptAdvice.getDespatchSupplierParty().getParty());
             receiptAdvice.getDespatchSupplierParty().setParty(supplierParty);
-            PartyType buyerParty = getParty(receiptAdvice.getDeliveryCustomerParty().getParty());
+            PartyType buyerParty = CatalogueDAOUtility.getParty(receiptAdvice.getDeliveryCustomerParty().getParty());
             receiptAdvice.getDeliveryCustomerParty().setParty(buyerParty);
 
             for (ReceiptLineType receiptLineType: receiptAdvice.getReceiptLine()){
-                PartyType manufacturerParty = getParty(receiptLineType.getItem().getManufacturerParty());
+                PartyType manufacturerParty = CatalogueDAOUtility.getParty(receiptLineType.getItem().getManufacturerParty());
                 receiptLineType.getItem().setManufacturerParty(manufacturerParty);
             }
 
@@ -112,37 +110,37 @@ public class DataIntegratorUtil {
         } else if(object instanceof TransportExecutionPlanRequestType) {
             TransportExecutionPlanRequestType transportExecutionPlanRequestType = (TransportExecutionPlanRequestType) object;
 
-            PartyType supplierParty = getParty(transportExecutionPlanRequestType.getTransportServiceProviderParty());
+            PartyType supplierParty = CatalogueDAOUtility.getParty(transportExecutionPlanRequestType.getTransportServiceProviderParty());
             transportExecutionPlanRequestType.setTransportServiceProviderParty(supplierParty);
-            PartyType buyerParty = getParty(transportExecutionPlanRequestType.getTransportUserParty());
+            PartyType buyerParty = CatalogueDAOUtility.getParty(transportExecutionPlanRequestType.getTransportUserParty());
             transportExecutionPlanRequestType.setTransportUserParty(buyerParty);
 
-            PartyType manufacturerParty = getParty(transportExecutionPlanRequestType.getMainTransportationService().getManufacturerParty());
+            PartyType manufacturerParty = CatalogueDAOUtility.getParty(transportExecutionPlanRequestType.getMainTransportationService().getManufacturerParty());
             transportExecutionPlanRequestType.getMainTransportationService().setManufacturerParty(manufacturerParty);
 
         } else if(object instanceof TransportExecutionPlanType) {
             TransportExecutionPlanType transportExecutionPlanType = (TransportExecutionPlanType) object;
 
-            PartyType supplierParty = getParty(transportExecutionPlanType.getTransportServiceProviderParty());
+            PartyType supplierParty = CatalogueDAOUtility.getParty(transportExecutionPlanType.getTransportServiceProviderParty());
             transportExecutionPlanType.setTransportServiceProviderParty(supplierParty);
-            PartyType buyerParty = getParty(transportExecutionPlanType.getTransportUserParty());
+            PartyType buyerParty = CatalogueDAOUtility.getParty(transportExecutionPlanType.getTransportUserParty());
             transportExecutionPlanType.setTransportUserParty(buyerParty);
 
         } else if(object instanceof ItemInformationRequestType) {
             ItemInformationRequestType itemInformationRequest = (ItemInformationRequestType) object;
 
-            PartyType supplierParty = getParty(itemInformationRequest.getSellerSupplierParty().getParty());
+            PartyType supplierParty = CatalogueDAOUtility.getParty(itemInformationRequest.getSellerSupplierParty().getParty());
             itemInformationRequest.getSellerSupplierParty().setParty(supplierParty);
-            PartyType senderParty = getParty(itemInformationRequest.getSenderParty());
+            PartyType senderParty = CatalogueDAOUtility.getParty(itemInformationRequest.getSenderParty());
             itemInformationRequest.setSenderParty(senderParty);
-            PartyType buyerParty = getParty(itemInformationRequest.getBuyerCustomerParty().getParty());
+            PartyType buyerParty = CatalogueDAOUtility.getParty(itemInformationRequest.getBuyerCustomerParty().getParty());
             itemInformationRequest.getBuyerCustomerParty().setParty(buyerParty);
-            PartyType receiverParty = getParty(itemInformationRequest.getReceiverParty());
+            PartyType receiverParty = CatalogueDAOUtility.getParty(itemInformationRequest.getReceiverParty());
             itemInformationRequest.setReceiverParty(receiverParty);
 
             for(ItemInformationRequestLineType requestLineType:itemInformationRequest.getItemInformationRequestLine()){
                 for (SalesItemType salesItemType: requestLineType.getSalesItem()){
-                    PartyType manufacturerParty = getParty(salesItemType.getItem().getManufacturerParty());
+                    PartyType manufacturerParty = CatalogueDAOUtility.getParty(salesItemType.getItem().getManufacturerParty());
                     salesItemType.getItem().setManufacturerParty(manufacturerParty);
                 }
             }
@@ -150,29 +148,15 @@ public class DataIntegratorUtil {
         } else if(object instanceof ItemInformationResponseType) {
             ItemInformationResponseType itemInformationResponse = (ItemInformationResponseType) object;
 
-            PartyType supplierParty = getParty(itemInformationResponse.getSellerSupplierParty().getParty());
+            PartyType supplierParty = CatalogueDAOUtility.getParty(itemInformationResponse.getSellerSupplierParty().getParty());
             itemInformationResponse.getSellerSupplierParty().setParty(supplierParty);
-            PartyType buyerParty = getParty(itemInformationResponse.getBuyerCustomerParty().getParty());
+            PartyType buyerParty = CatalogueDAOUtility.getParty(itemInformationResponse.getBuyerCustomerParty().getParty());
             itemInformationResponse.getBuyerCustomerParty().setParty(buyerParty);
 
             for (ItemType itemType:itemInformationResponse.getItem()){
-                PartyType manufacturerParty = getParty(itemType.getManufacturerParty());
+                PartyType manufacturerParty = CatalogueDAOUtility.getParty(itemType.getManufacturerParty());
                 itemType.setManufacturerParty(manufacturerParty);
             }
         }
     }
-
-    private static PartyType getParty(PartyType party){
-        if(party == null){
-            return null;
-        }
-        String query = "SELECT party FROM PartyType party WHERE party.ID = '"+party.getID()+"'";
-        PartyType partyType = HibernateUtility.getInstance(Configuration.UBL_PERSISTENCE_UNIT_NAME).load(query);
-        if(partyType == null){
-            HibernateUtility.getInstance(Configuration.UBL_PERSISTENCE_UNIT_NAME).persist(party);
-            return party;
-        }
-        return partyType;
-    }
-
 }
