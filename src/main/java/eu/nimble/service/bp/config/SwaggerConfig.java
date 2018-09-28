91f0a5e101e22f6c69a057259fe27050f6644b6d
dogukan10 <dogukan10cav@gmail.com>
2018-09-28 17:19:55 +0300

    Added api info to Swagger config

--- src/main/java/eu/nimble/service/bp/config/SwaggerConfig.java
+++ src/main/java/eu/nimble/service/bp/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +18,19 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("eu.nimble"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData(){
+        return new ApiInfo(
+                "NIMBLE Business Process REST API",
+                "REST API handling process instances on the NIMBLE platform",
+                "1.0",
+                "",
+                "",
+                "",
+                ""
+        );
     }
 }
