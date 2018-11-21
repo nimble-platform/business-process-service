8307fc294997bcb29a3c0dfed22fd4bde5755564
Suat Gonul <suat@srdc.com.tr>
2018-11-21 10:33:39 +0300

    added configs for ubldb and business process db

---
+++ src/main/java/eu/nimble/service/bp/config/BusinessProcessDBConfig.java
@@ -0,0 +1,71 @@
+package eu.nimble.service.bp.config;
+
+import eu.nimble.service.bp.impl.persistence.bp.BusinessProcessRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Map;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@EnableConfigurationProperties
+@PropertySource("classpath:bootstrap.yml")
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "bpdbEntityManagerFactory",
+        transactionManagerRef = "bpdbTransactionManager",
+        basePackages = {"eu.nimble.service.bp.impl.persistence.bp"}
+)
+@ComponentScan(basePackages = {"eu.nimble.service.bp.config"})
+public class BusinessProcessDBConfig {
+
+    @Bean(name = "bpdbDataSource")
+    @ConfigurationProperties(prefix = "persistence.orm.business_process.hibernate.connection")
+    public DataSource bpdbDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "bpdbEmfBean")
+    public LocalContainerEntityManagerFactoryBean bpdbEntityManagerFactoryBean(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("bpdbHibernateConfigs") Map hibernateConfigs,
+            @Qualifier("bpdbDataSource") DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean emfBean = builder
+                .dataSource(dataSource)
+                .persistenceUnit("bp-data-model")
+                .build();
+
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.putAll(hibernateConfigs);
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        emfBean.setJpaVendorAdapter(vendorAdapter);
+        emfBean.setJpaProperties(hibernateProperties);
+        return emfBean;
+    }
+
+    @Bean(name = "bpdbEntityManagerFactory")
+    public EntityManagerFactory bpdbEntityManagerFactory(@Qualifier("bpdbEmfBean") LocalContainerEntityManagerFactoryBean emfBean) {
+        return emfBean.getObject();
+    }
+
+    @Bean(name = "bpdbTransactionManager")
+    public PlatformTransactionManager bpdbTransactionManager(
+            @Qualifier("bpdbEntityManagerFactory") EntityManagerFactory bpdbEntityManagerFactory) {
+        return new JpaTransactionManager(bpdbEntityManagerFactory);
+    }
+}
