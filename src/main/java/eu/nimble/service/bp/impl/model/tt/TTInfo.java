55a630852507d54cf0e9fe36b36e553d2b7639bc
dogukan10 <dogukan10cav@gmail.com>
2019-06-14 13:18:56 +0300

    updated T&T info

--- src/main/java/eu/nimble/service/bp/impl/model/tt/TTInfo.java
+++ src/main/java/eu/nimble/service/bp/impl/model/tt/TTInfo.java
@@ -4,40 +4,31 @@ package eu.nimble.service.bp.impl.model.tt;
  * Created by suat on 06-Jun-18.
  */
 public class TTInfo {
-    private String eventUrl;
-    private String masterUrl;
-    private String productionProcessTemplate;
-    private String relatedProductId;
+    private String processInstanceId;
+    private String catalogueUuid;
+    private String catalogueLineHjid;
 
-    public String getEventUrl() {
-        return eventUrl;
+    public String getProcessInstanceId() {
+        return processInstanceId;
     }
 
-    public void setEventUrl(String eventUrl) {
-        this.eventUrl = eventUrl;
+    public void setProcessInstanceId(String processInstanceId) {
+        this.processInstanceId = processInstanceId;
     }
 
-    public String getMasterUrl() {
-        return masterUrl;
+    public String getCatalogueUuid() {
+        return catalogueUuid;
     }
 
-    public void setMasterUrl(String masterUrl) {
-        this.masterUrl = masterUrl;
+    public void setCatalogueUuid(String catalogueUuid) {
+        this.catalogueUuid = catalogueUuid;
     }
 
-    public String getProductionProcessTemplate() {
-        return productionProcessTemplate;
+    public String getCatalogueLineHjid() {
+        return catalogueLineHjid;
     }
 
-    public void setProductionProcessTemplate(String productionProcessTemplate) {
-        this.productionProcessTemplate = productionProcessTemplate;
-    }
-
-    public String getRelatedProductId() {
-        return relatedProductId;
-    }
-
-    public void setRelatedProductId(String relatedProductId) {
-        this.relatedProductId = relatedProductId;
+    public void setCatalogueLineHjid(String catalogueLineHjid) {
+        this.catalogueLineHjid = catalogueLineHjid;
     }
 }
--- src/main/java/eu/nimble/service/bp/impl/util/persistence/catalogue/CataloguePersistenceUtility.java
+++ src/main/java/eu/nimble/service/bp/impl/util/persistence/catalogue/CataloguePersistenceUtility.java
@@ -5,10 +5,10 @@ import eu.nimble.utility.persistence.JPARepositoryFactory;
 
 public class CataloguePersistenceUtility {
 
-    private static final String QUERY_SELECT_TT_DETAILS_BY_ID_AND_PARTY_ID = "SELECT cl.hjid,cl.goodsItem.item.trackAndTraceDetails.eventURL,cl.goodsItem.item.trackAndTraceDetails.masterURL,cl.goodsItem.item.trackAndTraceDetails.productionProcessTemplate FROM CatalogueLineType cl JOIN cl.goodsItem.item.manufacturerParty.partyIdentification partyIdentification WHERE cl.ID = :lineId AND partyIdentification.ID = :partyId";
+    private static final String QUERY_SELECT_TT_DETAILS_BY_ID_AND_PARTY_ID = "SELECT cl.hjid,cl.goodsItem.item.catalogueDocumentReference.ID FROM CatalogueLineType cl JOIN cl.goodsItem.item.manufacturerParty.partyIdentification partyIdentification WHERE cl.ID = :lineId AND partyIdentification.ID = :partyId";
     private static final String QUERY_SELECT_CAT_LINE_PARTY_ID_MANUFACTURER_ITEM_ID = "SELECT partyIdentification.ID,cl.goodsItem.item.manufacturersItemIdentification.ID FROM CatalogueLineType cl join cl.goodsItem.item.manufacturerParty.partyIdentification partyIdentification where cl.hjid = :hjid";
 
-    public static Object[] getCatalogueLineTTDetails(OrderType order) {
+    public static Object[] getCatalogueIdAndLineId(OrderType order) {
         return new JPARepositoryFactory().forCatalogueRepository(true).getSingleEntity(QUERY_SELECT_TT_DETAILS_BY_ID_AND_PARTY_ID,
                 new String[]{"lineId", "partyId"},
                 new Object[]{order.getOrderLine().get(0).getLineItem().getLineReference().get(0).getLineID(), order.getOrderLine().get(0).getLineItem().getItem().getManufacturerParty().getPartyIdentification().get(0).getID()});
