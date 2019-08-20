b4dec2e2fa20bfc9d48a74ab6772aaadc33e475d
dogukan10 <dogukan10cav@gmail.com>
2019-08-20 16:08:19 +0300

    mocked createChannel service

--- src/test/java/eu/nimble/service/bp/impl/mock/DataChannelClientMock.java
+++ src/test/java/eu/nimble/service/bp/impl/mock/DataChannelClientMock.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,5 +23,9 @@ public class DataChannelClientMock implements IDataChannelClient {
 
     public Response getEPCCodesForOrder(@RequestHeader("Authorization") String bearerToken, @PathVariable("code") String storeId) {
         return Response.builder().headers(new HashMap<>()).status(HttpStatus.OK.value()).body("[{\"orderId\":\"5b15c501-b90a-4f9c-ab0c-ca695e255237\",\"codes\":[\"2607191506\",\"2607191508\"]}]", Charset.defaultCharset()).build();
+    }
+
+    public Response createChannel(@RequestHeader("Authorization") String bearerToken, @RequestBody String request){
+        return Response.builder().headers(new HashMap<>()).status(HttpStatus.OK.value()).body("{\"channelID\":\"d03c3cfe-f369-496e-9ea9-fd8cb449c2c7-706-1339\"}", Charset.defaultCharset()).build();
     }
 }
