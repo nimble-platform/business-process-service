743b6fa277dce365c6a49c6532cb75edccaeedcc
dogukan10 <dogukan10cav@gmail.com>
2019-08-27 17:02:56 +0300

    migration script to delete duplicate documents

---
+++ src/main/java/eu/nimble/service/bp/util/migration/r13/R13MigrationController.java
@@ -0,0 +1,91 @@
+package eu.nimble.service.bp.util.migration.r13;
+
+import eu.nimble.utility.persistence.GenericJPARepository;
+import eu.nimble.utility.persistence.JPARepositoryFactory;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ApiIgnore
+@Controller
+public class R13MigrationController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final String QUERY_GET_DUPLICATE_IDS = "SELECT ID FROM %s GROUP BY ID HAVING count(ID) > 1";
+    private final String QUERY_GET_DOCUMENTS_BY_ID = "FROM %s WHERE ID = :id";
+
+    private final int LIMIT = 100;
+    private final int OFFSET = 1;
+
+    @ApiOperation(value = "", notes = "This script makes sure that each document has a unique identifier. It deletes the duplicate documents.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Deleted duplicate documents successfully"),
+            @ApiResponse(code = 401, message = "Invalid token. No user was found for the provided token")
+    })
+    @RequestMapping(value = "/r13/migration/remove-duplicates",
+            produces = {"application/json"},
+            method = RequestMethod.DELETE)
+    public ResponseEntity deleteDuplicateDocuments(@ApiParam(value = "The Bearer token provided by the identity service", required = true) @RequestHeader(value = "Authorization", required = true) String bearerToken
+    ) {
+        logger.info("Incoming request to delete duplicate documents");
+
+        // check token
+        ResponseEntity tokenCheck = eu.nimble.service.bp.util.HttpResponseUtil.checkToken(bearerToken);
+        if (tokenCheck != null) {
+            return tokenCheck;
+        }
+
+        GenericJPARepository repo = new JPARepositoryFactory().forCatalogueRepository(true);
+
+        List<String> tableNames = getTableNames();
+        for (String tableName : tableNames) {
+            // get duplicate ids
+            List<String> duplicateIds = repo.getEntities(String.format(QUERY_GET_DUPLICATE_IDS, tableName));
+            if (duplicateIds.size() > 0) {
+                logger.info("In table {}, duplicates exist for the documents with following ids: {}", tableName, duplicateIds);
+                for (String id : duplicateIds) {
+                    // remove duplicate documents
+                    List<Object> documents = repo.getEntities(String.format(QUERY_GET_DOCUMENTS_BY_ID, tableName), new String[]{"id"}, new Object[]{id}, LIMIT, OFFSET);
+                    for (Object document : documents) {
+                        repo.deleteEntity(document);
+                    }
+                }
+            }
+        }
+
+        logger.info("Completed request to delete duplicate documents");
+        return ResponseEntity.ok(null);
+    }
+
+    private List<String> getTableNames() {
+        List<String> tableNames = new ArrayList<>();
+
+        tableNames.add("ItemInformationRequestType");
+        tableNames.add("ItemInformationResponseType");
+        tableNames.add("RequestForQuotationType");
+        tableNames.add("QuotationType");
+        tableNames.add("PpapRequestType");
+        tableNames.add("PpapResponseType");
+        tableNames.add("OrderType");
+        tableNames.add("OrderResponseSimpleType");
+        tableNames.add("DespatchAdviceType");
+        tableNames.add("ReceiptAdviceType");
+        tableNames.add("TransportExecutionPlanRequestType");
+        tableNames.add("TransportExecutionPlanType");
+
+        return tableNames;
+    }
+}
