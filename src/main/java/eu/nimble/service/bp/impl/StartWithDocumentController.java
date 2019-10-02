0cd753822da4b0492d86076f05ab97b4915d8289
dogukan10 <dogukan10cav@gmail.com>
2019-10-02 11:43:52 +0300

    accepted 204 return code while sending quotation

--- src/main/java/eu/nimble/service/bp/impl/StartWithDocumentController.java
+++ src/main/java/eu/nimble/service/bp/impl/StartWithDocumentController.java
@@ -276,7 +276,7 @@ public class StartWithDocumentController {
                             .body(msg)
                             .asString();
 
-                    if(response.getStatus() != 200){
+                    if(response.getStatus() != 200 && response.getStatus() != 204){
                         logger.error("Failed send the document to the initiator party {}, endpoint: {} : {}",initiatorParty.getPartyIdentification().get(0).getID(), communicationChannel.getValue(), response.getBody());
                         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
                     }
