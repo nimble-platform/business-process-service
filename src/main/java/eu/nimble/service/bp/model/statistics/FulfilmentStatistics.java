0000f3b0cc4751c448e4548934e0c20377415a92
dogukan10 <dogukan10cav@gmail.com>
2019-10-16 10:55:55 +0300

    REST service to get fulfilment statistics for an order

---
+++ src/main/java/eu/nimble/service/bp/model/statistics/FulfilmentStatistics.java
@@ -0,0 +1,34 @@
+package eu.nimble.service.bp.model.statistics;
+
+import java.math.BigDecimal;
+
+public class FulfilmentStatistics {
+
+    private BigDecimal dispatchedQuantity;
+    private BigDecimal rejectedQuantity;
+    private BigDecimal requestedQuantity;
+
+    public BigDecimal getDispatchedQuantity() {
+        return dispatchedQuantity;
+    }
+
+    public void setDispatchedQuantity(BigDecimal dispatchedQuantity) {
+        this.dispatchedQuantity = dispatchedQuantity;
+    }
+
+    public BigDecimal getRejectedQuantity() {
+        return rejectedQuantity;
+    }
+
+    public void setRejectedQuantity(BigDecimal rejectedQuantity) {
+        this.rejectedQuantity = rejectedQuantity;
+    }
+
+    public BigDecimal getRequestedQuantity() {
+        return requestedQuantity;
+    }
+
+    public void setRequestedQuantity(BigDecimal requestedQuantity) {
+        this.requestedQuantity = requestedQuantity;
+    }
+}
