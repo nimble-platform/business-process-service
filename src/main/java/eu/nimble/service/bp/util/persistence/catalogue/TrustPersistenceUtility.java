b7885e9a0fba8769aa55ba054a45377a8b34f8e4
dogukan10 <dogukan10cav@gmail.com>
2019-12-30 17:26:48 +0300

    fixed some bugs in TrustPersistenceUtility

--- src/main/java/eu/nimble/service/bp/util/persistence/catalogue/TrustPersistenceUtility.java
+++ src/main/java/eu/nimble/service/bp/util/persistence/catalogue/TrustPersistenceUtility.java
@@ -113,7 +113,12 @@ public class TrustPersistenceUtility {
         }
 
         qualifyingParty.getCompletedTask().add(completedTask);
-        BusinessProcessContextHandler.getBusinessProcessContextHandler().getBusinessProcessContext(businessContextId).getCatalogRepository().updateEntity(qualifyingParty);
+        if(businessContextId == null){
+            new JPARepositoryFactory().forCatalogueRepository().updateEntity(qualifyingParty);
+        }
+        else {
+            BusinessProcessContextHandler.getBusinessProcessContextHandler().getBusinessProcessContext(businessContextId).getCatalogRepository().updateEntity(qualifyingParty);
+        }
     }
 
     public static void createCompletedTasksForBothParties(String processInstanceID,String bearerToken,String status, String businessContextId) {
@@ -149,7 +154,7 @@ public class TrustPersistenceUtility {
         for (CompletedTaskType completedTask:completedTasks){
 
             // consider only Completed tasks
-            if(completedTask.getDescription().get(0).equals("Completed")){
+            if(completedTask.getDescription().get(0).getValue().contentEquals("Completed")){
                 List<EvidenceSuppliedType> ratings = new ArrayList<>();
                 List<CommentType> reviews = new ArrayList<>();
 
