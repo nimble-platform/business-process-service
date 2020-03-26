ed37d5ba148ea23963ee7f286b1caf4e772b7579
dogukan10 <dogukan10cav@gmail.com>
2020-03-26 15:22:19 +0300

    REST service to retrieve an invoice

--- src/main/java/eu/nimble/service/bp/util/persistence/catalogue/PaymentPersistenceUtility.java
+++ src/main/java/eu/nimble/service/bp/util/persistence/catalogue/PaymentPersistenceUtility.java
@@ -11,26 +11,41 @@ import org.json.JSONObject;
 import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 public class PaymentPersistenceUtility {
 
     private static String QUERY_CHECK_EXISTENCE_OF_INVOICE = "SELECT count(*) FROM InvoiceType invoice WHERE invoice.orderReference.documentReference.ID = :orderId";
+    private static String QUERY_GET_INVOICE = "SELECT invoice FROM InvoiceType invoice WHERE invoice.orderReference.documentReference.ID = :orderId";
 
     public static boolean isPaymentDoneForOrder(String orderId){
         Long size = new JPARepositoryFactory().forCatalogueRepository().getSingleEntity(QUERY_CHECK_EXISTENCE_OF_INVOICE,new String[]{"orderId"},new Object[]{orderId});
         return size > 0;
     }
 
-    public static InvoiceType createInvoiceForOrder(String orderId){
-        DocumentReferenceType documentReference = new DocumentReferenceType();
-        documentReference.setID(orderId);
+    public static InvoiceType getInvoiceForOrder(String orderId){
+        return new JPARepositoryFactory().forCatalogueRepository(true).getSingleEntity(QUERY_GET_INVOICE,new String[]{"orderId"},new Object[]{orderId});
+    }
+
+    public static InvoiceType createInvoiceForOrder(String orderId, String invoiceId, String invoiceUrl){
+        DocumentReferenceType orderDocumentReference = new DocumentReferenceType();
+        orderDocumentReference.setID(orderId);
 
         OrderReferenceType orderReference = new OrderReferenceType();
-        orderReference.setDocumentReference(documentReference);
+        orderReference.setDocumentReference(orderDocumentReference);
 
         InvoiceType invoice = new InvoiceType();
         invoice.setOrderReference(orderReference);
+        if(invoiceId != null){
+            invoice.setID(invoiceId);
+        }
+        if(invoiceUrl != null){
+            DocumentReferenceType invoiceUrlDocumentReference = new DocumentReferenceType();
+            invoiceUrlDocumentReference.setID(invoiceUrl);
+
+            invoice.setOriginatorDocumentReference(Arrays.asList(invoiceUrlDocumentReference));
+        }
 
         return invoice;
     }
